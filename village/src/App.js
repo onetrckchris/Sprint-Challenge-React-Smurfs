9fa4a818e13e179be30b2f36bd2eda15ecdecaab
onetrckchris <onetrckchris@gmail.com>
2019-06-14 08:49:19 -0700

    listing all smurfs with axios get request and beginning to route all components

--- village/src/App.js
+++ village/src/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import { Route } from 'react-router-dom';
 
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
+import Navbar from './components/Navbar';
 
 class App extends Component {
   constructor(props) {
@@ -11,14 +14,47 @@ class App extends Component {
       smurfs: [],
     };
   }
+
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
+
+  componentDidMount() {
+    axios.get('http://localhost:3333/smurfs')
+    .then(response => {
+      console.log(response);
+      this.setState({ smurfs: response.data });
+    })
+    .catch(error => {
+      console.log(error);
+    })
+  }
+
+  addSmurf = newSmurf => {
+    axios.post('http://localhost:3333/smurfs', newSmurf)
+    .then(response => {
+      console.log(response);
+      this.setState({ smurfs: response.data });
+    })
+    .catch(error => {
+      console.log(error);
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <SmurfForm />
-        <Smurfs smurfs={this.state.smurfs} />
+        <Navbar />
+
+        <Route exact path="/smurf-form" render={props => 
+            <SmurfForm {...props} addSmurf={this.addSmurf} />
+          }>
+        </Route>
+
+        <Route exact path="/" render={props =>
+            <Smurfs {...props} smurfs={this.state.smurfs} />
+          }>
+        </Route>
       </div>
     );
   }
---
+++ village/src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Navbar = () => {
+    return (
+        <nav>
+            
+        </nav>
+    )
+}
+
+export default Navbar;
